446690e07282b950a38a685fa0e34e32cef42406
Margie Roswell <mroswell@gmail.com>
2014-06-30 14:43:27 -0400

    remove bold if Windows platform

--- checkspace.py
+++ checkspace.py
@@ -25,13 +25,19 @@ def process(input_voter_file, input_poll_file):
     input_files_size = int((os.path.getsize(input_poll_file) + os.path.getsize(input_voter_file)))
     bold_input_files_size = BOLD + str(int(input_files_size)) + END
     # parsed_output_files_size = os.path.getsize('parsed_voter_file.csv') + os.path.getsize('parsed_precinct_polling_list.csv')
-    parsed_estimated_output_files_size = int(input_files_size * 1.19)
-    bold_estimated_parsed_output_files_size = BOLD + str(parsed_estimated_output_files_size) + END
+    estimated_parsed_output_files_size = int(input_files_size * 1.19)
+    bold_estimated_parsed_output_files_size = BOLD + str(estimated_parsed_output_files_size) + END
     # output_files_size = os.path.getsize('parsed_voter_file.csv') + os.path.getsize('parsed_precinct_polling_list.csv') + os.path.getsize('voter_poll_joined_fictional.csv')
     estimated_output_files_size = int(input_files_size * 2.4)
     bold_estimated_output_files_size = BOLD + str(estimated_output_files_size) + END
     # print estimated_output_files_size, output_files_size, parsed_output_files_size
     bold_freespace = BOLD + str(get_free_space_bytes('/')) + END
+    if platform.system() == 'Windows':
+        bold_freespace = str(get_free_space_bytes)
+        bold_estimated_output_files_size = str(estimated_output_files_size)
+        bold_estimated_parsed_output_files_size = str(estimated_parsed_output_files_size)
+        bold_input_files_size = str(input_files_size)
+
     print "------------------------"
     print "SUMMARY"
     print "voter_poll_join.py will require approximately {} bytes of additional storage".format(bold_estimated_output_files_size)
